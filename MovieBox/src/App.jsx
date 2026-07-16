0eb489209cd892238ca4b506d2af813be123f604
Lorrany <lorrany.acopiara@gmail.com>
2026-07-16 00:47:27 -0300

    configurando o react e o tailwind

---
+++ MovieBox/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Teste from './components/teste'
+
+function App() {
+  return (
+    <div>
+      <Teste />
+    </div>
+  )
+}
+
+export default App
---
+++ MovieBox/src/components/teste.jsx
@@ -0,0 +1,10 @@
+function Teste() {
+  return (
+    <div className="p-6 rounded-xl bg-slate-800 shadow-lg max-w-md mx-auto mt-10">
+      <h1 className="text-2xl font-bold text-cyan-400 mb-2">bloblobli</h1>
+      <p className="text-slate-300">Este conteúdo agora está sendo renderizado com Tailwind.</p>
+    </div>
+  )
+}
+
+export default Teste
